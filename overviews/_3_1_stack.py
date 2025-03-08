501de403fe481cac8fdbe698a4975f591d32e3ad
Kriss Andrew <Kriss1Andrew@gmail.com>
2025-03-08 12:42:18 +1000

    added first stack test

--- overviews/_3_1_stack.py
+++ overviews/_3_1_stack.py
@@ -2,6 +2,7 @@ class Stack:
     
     def __init__(self):
         self.stack = []
+        self.max_values = []
 
     # O(1) running time
     def is_empty(self):
@@ -9,16 +10,23 @@ class Stack:
 
     # O(1) running time
     def push(self, data):
+        if self.is_empty():
+            self.max_values.append(data) # If empty value will always be max
+
+        elif data >= self.max_values[-1]:
+            self.max_values.append(data)
         self.stack.append(data)
 
     # O(1) because we manipulate the last item
     def pop(self):
 
-        if self.size_stack < 1:
+        if self.is_empty():
             return None
-
+        
         data = self.stack[-1]
         del self.stack[-1]
+        if self.max_values[-1] == data:
+            del self.max_values[-1]
         return data
 
     # O(1) constant running time
@@ -28,4 +36,15 @@ class Stack:
     # O(1) - python arrays track a length characteristic
     def size_stack(self):
         return len(self.stack)
+    
+    def max_value(self):
+        if not self.is_empty():
+            return self.max_values[-1]
+        return None
+    
+    # O(n) - needs to assign new node items for each item in provided array
+    def generate_stack_from_array(self, array: list[int]):
+        for data in array:
+            self.push(data)
+        return True
     
